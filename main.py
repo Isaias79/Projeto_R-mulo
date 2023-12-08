8ad6808469d217d10082797953f59154dd77fbe1
Isaias79 <60967597+Isaias79@users.noreply.github.com>
2023-12-08 19:49:58 +0000

    Testes

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+'''
+Para valores menores que um Valor Estipulado para aquele
+celular pode aparecer um aviso que é ou pode ser golpe
+'''
+
 pesquisa = input("Digite O Que Você Quer Pesquisar: ")
 
 url = f'https://www.google.com/search?q={pesquisa}&sca_esv=587264773&tbm=shop&source=lnms&sa=X&ved=2ahUKEwi5y4Kp0fCCAxVCqpUCHQ6KB0YQ_AUoAXoECAIQAw&biw=1242&bih=619&dpr=1.1&gl=BR'
@@ -18,14 +23,14 @@ classe_produto = primeiro_produto.get('class')[0]
 produtos = soup.findAll('div', { "class": classe_produto })
 # Procura todas as tags div com a mesma classe que a tag anterior
 
-'''
+# IMPORTANDO NO CÓDIGO HTML
 def importar(): 
         with open('pg.html', 'w', encoding='utf-8') as file:
             file.write(str(resposta.content))
         print("Código HTML escrito com sucesso e salvo como 'pg.html'")
 
 importar()  
-'''
+
 
 lista = {}
 lista2 = []
@@ -63,7 +68,7 @@ for chave, valor in lista.items():
             
     else:
         remover = chave
-        print(f"Chave {chave}: Números não encontrados na string")
+        #print(f"Chave {chave}: Números não encontrados na string")
 
 lista.pop(remover)
 lista2 = sorted(lista2)
@@ -79,7 +84,7 @@ for i in range(-10, 0, 1):
 #print('\n |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||', '\n \n')
 #print('O Produto mais barato é: ', menor, '\n')
 print('TOP - 10 \n ')
-print(' -', lista2[-1], '\n')
+print(' -',  lambda lista2: {lista2[-1]} if lista2[-1] <= 600 and lista2[-1] >= 300 else None, lista[-1], '\n') # VALOR ESTIPULADO
 print(' -', lista2[-2], '\n')
 print(' -', lista2[-3], '\n')
 print(' -', lista2[-4], '\n')
@@ -89,6 +94,7 @@ print(' -', lista2[-7], '\n')
 print(' -', lista2[-8], '\n')
 print(' -', lista2[-9], '\n')
 print(' -', lista2[-10], '\n')
+
 
 lista2 = lista.items() # Adicionar uma lista pra ir removendo e ir deixando nos top 10 minimos
 
